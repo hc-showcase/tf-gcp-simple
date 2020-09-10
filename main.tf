9fb8f144e2de5de4810fbc583e7dc3e0eb9a5496
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2020-09-10 12:48:22 +0200

    init

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "google_compute_network" "default" {
 
 resource "google_compute_instance" "tfe" {
   name         = "tfe"
-  machine_type = "n1-standard-4"
+  machine_type = "n1-standard-2"
   zone         = "europe-west3-a"
   hostname     = "tfe.msk.pub"
 
