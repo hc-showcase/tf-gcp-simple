5e5bd87e20c7d65002847c2d3ee9066dc3a7ca81
mkaesz <mkaesz@users.noreply.github.com>
2020-09-25 10:23:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,23 +40,14 @@ resource "google_compute_instance" "tfe" {
   }
 }
 
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
-  force_destroy = true
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
 
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+resource "google_storage_bucket" "bucket" {
+  name = "static-content-bucket"
 }
 
 output "tfe" {
