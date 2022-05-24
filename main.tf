bb5c60e35cfec7cef3d14f377be1edee7d5ee868
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-05-24 13:52:12 +0200

    init

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
-variable "project" {
-  default = "mkaesz" 
+variable "server_port" {
+   description = "The server port"
+   type        = number
 }
 
 provider "google" {
-##  credentials = file("~/.config/gcloud/application_default_credentials.json")
-  project     = var.project
+  project     = "msk-pub"
 }
 
-data "google_compute_network" "default" {
-  name = "default"
+resource "google_compute_network" "example_network" {
+  name                    = "example-network"
+  auto_create_subnetworks = "true"
 }
 
 resource "google_compute_instance" "vm" {
@@ -17,27 +18,49 @@ resource "google_compute_instance" "vm" {
   zone         = "europe-west3-a"
   hostname     = "vm0815.msk.pub"
 
-  tags = ["vm0815", "manual"]
+  tags = ["web"]
 
   boot_disk {
     initialize_params {
-      image = "centos-7-v20200618"
-      size  = 100
+      image = "ubuntu-2110-impish-v20220505"
+      size  = 10
       type  = "pd-ssd"
     }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.example_network.name
 
     access_config {
       // Ephemeral IP
     }
   }
 
+  metadata_startup_script = <<EOT
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p ${var.server_port} &
+EOT
+
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "firewall"
+  network = google_compute_network.example_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["${var.server_port}"]
+  }
+
+  target_tags = ["web"]
 }
 
 resource "google_storage_bucket_access_control" "public_rule" {
@@ -48,8 +71,10 @@ resource "google_storage_bucket_access_control" "public_rule" {
 
 resource "google_storage_bucket" "bucket" {
   name = "static-content-bucket-msk-0815"
+  location = "EU"
 }
 
 output "vm" {
-  value = google_compute_instance.vm
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  sensitive = true
 }
