e19caf767a64f86cbdbfe91c0be4b5306169faf8
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-05-24 15:26:37 +0200

    init

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.9.0"
+    }
+  }
+}
+
 variable "server_port" {
    description = "The server port"
    type        = number
